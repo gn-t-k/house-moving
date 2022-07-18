7fa1c7341d89e04ed863042ecbc03fee78b96311
gn-t-k <gntk1995422@gmail.com>
2022-07-18 11:42:39 +0900

    ワークアウト登録ページ

--- src/features/workout/register-workout/register-workout.tsx
+++ src/features/workout/register-workout/register-workout.tsx
@@ -1,4 +1,13 @@
-import { Input, Stack, Button, Select } from "@chakra-ui/react";
+import {
+  Input,
+  Stack,
+  Button,
+  Select,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Textarea,
+} from "@chakra-ui/react";
 import { FC, MouseEventHandler } from "react";
 import { useFieldArray } from "react-hook-form";
 import { defaultValues, useWorkoutForm } from "../use-workout-form";
@@ -7,58 +16,108 @@ import { Exercise } from "@/features/exercise/exercise";
 type Props = {
   exercises: Exercise[];
 };
-
 export const RegisterWorkout: FC<Props> = (props) => {
   const {
     control,
     register,
     formState: { errors },
   } = useWorkoutForm();
-  console.log({ errors });
   const { fields, append, remove } = useFieldArray({
     control,
     name: "workoutSets",
   });
 
-  const handleAppend: MouseEventHandler<HTMLButtonElement> = (_e) => {
+  const isLastField = fields.length === 1;
+
+  const appendField: MouseEventHandler<HTMLButtonElement> = (_e) => {
     append(defaultValues);
   };
-  const handleRemove =
+  const removeField =
     (index: number): MouseEventHandler<HTMLButtonElement> =>
     (_e) => {
+      if (isLastField) {
+        return;
+      }
+
       remove(index);
     };
-  const a = { ...register(`workoutSets.${0}.memo`) };
-  a.disabled;
 
   return (
     <Stack direction="column">
       {fields.map((field, index) => (
         <Stack key={field.id} direction="row">
           <Stack direction="column">
-            <Select placeholder="種目を選択">
-              {props.exercises.map((exercise) => (
-                <option key={exercise.id} value={exercise.id}>
-                  {exercise.name}
-                </option>
-              ))}
-            </Select>
-            <Input
-              {...register(`workoutSets.${index}.weight`, {
-                required: true,
-                validate: {
-                  number: (value) =>
-                    !Number.isNaN(parseInt(value)) || "数字を入力してください",
-                },
-              })}
-            />
-            <p>{errors.workoutSets?.[index]?.weight?.message}</p>
-            <Input {...register(`workoutSets.${index}.memo`)} />
+            <FormControl
+              isInvalid={errors.workoutSets?.[index]?.exerciseId !== undefined}
+            >
+              <FormLabel>種目を選択</FormLabel>
+              <Select
+                placeholder="未選択"
+                {...register(`workoutSets.${index}.exerciseId`, {
+                  required: { value: true, message: "入力必須です" },
+                })}
+              >
+                {props.exercises.map((exercise) => (
+                  <option key={exercise.id} value={exercise.id}>
+                    {exercise.name}
+                  </option>
+                ))}
+              </Select>
+              <FormErrorMessage>
+                {errors.workoutSets?.[index]?.exerciseId?.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl
+              isInvalid={errors.workoutSets?.[index]?.weight !== undefined}
+            >
+              <FormLabel>重量</FormLabel>
+              <Input
+                {...register(`workoutSets.${index}.weight`, {
+                  required: {
+                    value: true,
+                    message: "入力必須です",
+                  },
+                  validate: {
+                    number: (value: string) =>
+                      /^[0-9]*$/.test(value) || "数字を入力してください",
+                  },
+                })}
+              />
+              <FormErrorMessage>
+                {errors.workoutSets?.[index]?.weight?.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl
+              isInvalid={errors.workoutSets?.[index]?.repetition !== undefined}
+            >
+              <FormLabel>回数</FormLabel>
+              <Input
+                {...register(`workoutSets.${index}.repetition`, {
+                  required: {
+                    value: true,
+                    message: "入力必須です",
+                  },
+                  validate: {
+                    number: (value: string) =>
+                      /^[0-9]*$/.test(value) || "数字を入力してください",
+                  },
+                })}
+              />
+              <FormErrorMessage>
+                {errors.workoutSets?.[index]?.repetition?.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl>
+              <FormLabel>メモ</FormLabel>
+              <Textarea {...register(`workoutSets.${index}.memo`)} />
+            </FormControl>
           </Stack>
-          <Button onClick={handleRemove(index)}>削除</Button>
+          <Button onClick={removeField(index)} disabled={isLastField}>
+            削除
+          </Button>
         </Stack>
       ))}
-      <Button onClick={handleAppend}>追加</Button>
+      <Button onClick={appendField}>追加</Button>
     </Stack>
   );
 };
